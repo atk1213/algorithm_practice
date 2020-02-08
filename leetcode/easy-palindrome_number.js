dcbc8b49066a86c8dce6e0fa9903edeb19f66483
Anthony Kim <anthonykim@ganthonykim.lan>
2020-02-07 21:59:14 -0800

    init

---
+++ leetcode/easy-palindrome_number.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  // init result number variable, temp variable, and input value
+  var revNum = 0;
+  var temp = 0;
+  var input = x;
+  // if input is 0
+  if (x === 0){
+      return true
+  // check for negative sign or a value that ends in 0
+  } else if (x < 0 || x % 10 === 0){
+      return false
+  }
+  // use modulo to take last value and add it to revNum while manipulated input is > 0
+  while (input !== 0){
+    // get ones value and store to temp variable
+    temp = input % 10;
+    // multiply revNum by ten and add temp value
+    revNum = revNum * 10 + temp
+    // floor float value to get positive integer
+    input = Math.floor(input/10)
+  }
+  // return boolean of comparison
+  return x === revNum    
+};
