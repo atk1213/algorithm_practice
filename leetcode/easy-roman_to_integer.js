8b4940f07a6ef3957a4f7be8c72fbbc31d74d4a8
Anthony Kim <anthonykim@ganthonykim.lan>
2020-02-11 23:36:56 -0800

    [prog] completed prompt and included refactor

---
+++ leetcode/easy-roman_to_integer.js
@@ -0,0 +1,64 @@
+/*
+Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+var roman = {
+  "I": 1,
+  "V": 5,
+  "X": 10,
+  "L": 50,
+  "C": 100,
+  "D": 500,
+  "M": 1000
+}
+
+// var romanToInt = function(s) {
+//   var num = 0;
+//   for (var charIndex = 0; charIndex <= s.length-1; charIndex++){
+//     if (roman[s[charIndex]] == 1 && roman[s[charIndex+1]] == 5){
+//       num += 4;
+//       charIndex++
+//     } else if (roman[s[charIndex]] == 1 && roman[s[charIndex+1]] == 10){
+//       num += 9;
+//       charIndex++
+//     } else if (roman[s[charIndex]] == 10 && roman[s[charIndex+1]] == 50){
+//       num += 40;
+//       charIndex++
+//     } else if (roman[s[charIndex]] == 10 && roman[s[charIndex+1]] == 100){
+//       num += 90;
+//       charIndex++
+//     } else if (roman[s[charIndex]] == 100 && roman[s[charIndex+1]] == 500){
+//       num += 400;
+//       charIndex++
+//     } else if (roman[s[charIndex]] == 100 && roman[s[charIndex+1]] == 1000){
+//       num += 900;
+//       charIndex++
+//     } else {
+//       num += roman[s[charIndex]]
+//     }
+//   }
+//   return num
+// };
+
+var romanToInt = function(s) {
+  var num = 0;
+  for (var charIndex = 0; charIndex <= s.length-1; charIndex++){
+    if (roman[s[charIndex]] < roman[s[charIndex+1]]){
+      num += roman[s[charIndex+1]] - roman[s[charIndex]];
+      charIndex++
+    } else if (roman[s[charIndex]] == 1 && roman[s[charIndex+1]] == 10){
+      num += 9;
+      charIndex++
+    } else {
+      num += roman[s[charIndex]]
+    }
+  }
+  return num
+};
+
+console.log(romanToInt("MCMXCIV"))
