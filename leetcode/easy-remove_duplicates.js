0c1bc83053e5f0d714261f13dfb6e1b08460b334
Anthony Kim <anthonykim@ganthonykim.lan>
2020-02-13 00:54:47 -0800

    [prog] completed prompt and refactored to better solution

---
+++ leetcode/easy-remove_duplicates.js
@@ -0,0 +1,34 @@
+/*
+Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+*/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// var removeDuplicates = function(nums) {
+//   var prev;
+//   for (var valInd = 0; valInd < nums.length; valInd++){
+//       if (prev == undefined){
+//           prev = nums[valInd]
+//       } else if (prev == nums[valInd]){
+//           while (prev == nums[valInd]){
+//               nums.splice(valInd, 1);
+//           }
+//       } 
+//       prev = nums[valInd]
+//   }
+// };
+
+var removeDuplicates = function(nums) {
+  var index = 0;
+  for (var valInd = 1; valInd < nums.length; valInd++){
+      if (nums[valInd] == nums[valInd - 1]){
+          index++
+      } else {
+          nums[valInd-index] = nums[valInd]
+      }
+  }
+  nums.splice(nums.length-index)
+  return nums.length
+};
