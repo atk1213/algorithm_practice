d5d468b1cac598130cc7d8e69f38b934a3acc101
Anthony Kim <akim2214@gmail.com>
2020-05-01 23:38:03 -0700

    [prog] completed same tree prompt & refactored

---
+++ leetcode/easy-same_tree.js
@@ -0,0 +1,36 @@
+/*
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+
+// var isSameTree = function(p, q) {
+//   if(!p && !q){
+//       return true   
+//   } else if ((!p && q)||(p && !q)){
+//       return false
+//   } else if((p && q)&&(p.val === q.val)){
+//       return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+//   } else {
+//       return false
+//   }
+//   return true
+// };
+
+var isSameTree = function(p, q) {
+  if (!p && !q) return true;
+  if (!p || !q || p.val !== q.val) return false;
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
